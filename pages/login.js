c8a9f4e97ffba7177d65c02618555f11b15b47bc
Thetribunalcode <ullasrajesh23@gmail.com>
2023-02-18 04:41:22 +0530

    Added arcana implementation

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import Metamask from "../components/metamask";
 import { useAccount, useConnect } from "wagmi";
@@ -9,24 +10,17 @@ import Toast from "../components/toast";
 export default function Login() {
   const router = useRouter();
   const { connect, error, isLoading } = useConnect({
-    connector: new InjectedConnector({
-      options: {
-        shimDisconnect: true,
-      },
-    }),
+    connector: new InjectedConnector(),
   });
 
-  const { isConnected, address } = useAccount();
+  const { isConnected } = useAccount();
 
   useEffect(() => {
     if (isConnected) {
       // If wallet is already connected, push them to dashboard
       console.log(isConnected ? "Connected" : "Not connected");
-      // Check if owner
-      console.log(address, process.env.NEXT_PUBLIC_OWNER_ADDRESS);
-      if (address === process.env.NEXT_PUBLIC_OWNER_ADDRESS)
-        router.push("/admin/dashboard");
-      else router.push("/user/dashboard");
+      // Modify dashboard component and put that route here
+      router.push("/user/dashboard");
     }
   }, []);
 
