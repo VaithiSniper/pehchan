737cfb60cb6cb94962e2cc4fef801f9e68ec9614
VaithiSniper <vaithi.genghiskhan@gmail.com>
2023-02-18 17:27:50 +0530

    admin candidate view done

--- contracts/Candidate.sol
+++ contracts/Candidate.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 contract Candidate {
 
+    // status of application of candidate
     enum applicationStatus{
         initalDefault,
         In_Review,
@@ -10,29 +11,28 @@ contract Candidate {
         Approved     
     }
 
-    struct candidateDetails {
-        uint256 candidateID;
-        applicationStatus status;
-    }
-
+    //stores metadata of candidate
     struct candidateMetadata {
         string name;
         string party;
         uint age;
+        address candidateAddress;
+        applicationStatus status;
     }
 
+    // keep track of owner and candidatesCount 
     address payable public owner;
     uint256 public candidatesCount;
-
+    // initalise owner and candidatesCount in constructor
     constructor() {
         candidatesCount = 0;
         owner = payable(msg.sender);
     }
 
-    mapping(address => candidateDetails) candidates;
-    // uint candidateDetailsArraySize = 127;
-    // candidateMetadata[] public candidateMetadataArray = new candidateMetadata[](candidateDetailsArraySize);
-    candidateMetadata[] public candidateMetadataArray;
+    // serves as a mapping from address to current candidateID that is used to identify candidates
+    mapping(address => uint256) candidates;
+    // stores corresponing metadata of candidate in corresponding array, at candidateID index position
+    candidateMetadata[] candidateMetadataArray;
 
     event candidateStatus(address candidateAddress, applicationStatus);
     event candidateRemoved(address candidateAddress);
@@ -43,34 +43,33 @@ contract Candidate {
     //     _;
     // }
 
-    function addCandidate(address _candidateAddress, string memory _name, string memory _party, uint _age) public {
+    function addCandidate(address _candidateAddress, string memory _name, string memory _party, uint8 _age) public {
         // check if application has already been sent
-        require(candidates[_candidateAddress].status != applicationStatus.initalDefault,  "This candidate has already applied");
-        // move the application status to in review
-        candidates[_candidateAddress].status = applicationStatus(uint(candidates[_candidateAddress].status)+1);
-        // set candidateid as the i// check if application has already been sentndex
-        candidates[_candidateAddress].candidateID = candidatesCount;
-        // set the array's corresponding index with this data
-        candidateMetadataArray.push(candidateMetadata(_name, _party, _age));
+        require(candidates[_candidateAddress] == 0,  "This candidate has already applied");
+        // first set the candidateID as value for that address in mapping
+        candidates[_candidateAddress] = candidatesCount;
+        // set the array's corresponding index with the candidate metadata
+        candidateMetadataArray.push(candidateMetadata(_name, _party, _age, _candidateAddress, applicationStatus.In_Review));
         // increment the index
         candidatesCount++;
         // emit event regarding current status for push
-        emit candidateStatus(_candidateAddress, candidates[_candidateAddress].status);
+        emit candidateStatus(_candidateAddress, applicationStatus.In_Review);
     }
 
     function upgradeCandidate(address _candidateAddress) public {
         // check if already approved
-        require(candidates[_candidateAddress].status == applicationStatus.Approved,  "This candidate is already approved");
+        require(candidateMetadataArray[candidates[_candidateAddress]].status != applicationStatus.Approved,  "This candidate is already approved");
         // upgrade status
-        candidates[_candidateAddress].status = applicationStatus(uint(candidates[_candidateAddress].status)+1);
+        candidateMetadataArray[candidates[_candidateAddress]].status = applicationStatus(uint(candidateMetadataArray[candidates[_candidateAddress]].status)+1);
         // emit event regarding current status for push
-        emit candidateStatus(_candidateAddress, candidates[_candidateAddress].status);
+        emit candidateStatus(_candidateAddress, candidateMetadataArray[candidates[_candidateAddress]].status);
     }
 
     function removeCandidate(address _candidateAddress) public {
         // check if candidate exists
-        require(candidates[_candidateAddress].status == applicationStatus.initalDefault,  "This candidate doesn't exist");
+        require(candidateMetadataArray[candidates[_candidateAddress]].status != applicationStatus.initalDefault,  "This candidate doesn't exist");
         // delete the data, might get pruned
+        delete candidateMetadataArray[candidates[_candidateAddress]];
         delete candidates[_candidateAddress];
         // emit event regarding deletion for push
         emit candidateRemoved(_candidateAddress);
@@ -78,13 +77,24 @@ contract Candidate {
 
     function getDataOfCandidate(address _candidateAddress) public view returns(address, candidateMetadata memory) {
         // check if candidate exists
-        require(candidates[_candidateAddress].status == applicationStatus.initalDefault,  "This candidate doesn't exist");
+        require(candidateMetadataArray[candidates[_candidateAddress]].status != applicationStatus.initalDefault,  "This candidate doesn't exist");
         // return candidate details
-        return (_candidateAddress, candidateMetadataArray[candidates[_candidateAddress].candidateID]);
+        return (_candidateAddress, candidateMetadataArray[candidates[_candidateAddress]]);
+    }
+
+    function getDataOfAllCandidates() public view returns(candidateMetadata[] memory) {
+        // check if candidates even exist
+        // require(candidateMetadataArray[0],  "This candidate doesn't exist");
+        // return candidate details
+        return (candidateMetadataArray);
     }
 
     function getStatusOfCandidate(address _candidateAddress) public view returns(address, applicationStatus) {
-        return (_candidateAddress, candidates[_candidateAddress].status);
+        return (_candidateAddress, candidateMetadataArray[candidates[_candidateAddress]].status);
+    }
+
+    function isMetatdataArrayEmpty() public view returns(bool) {
+        return (candidateMetadataArray.length == 0 ? true : false);
     }
 
 }
